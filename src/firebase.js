6e06b7e77a9128e060ac33654f1f40552a02c9ac
wt854 <wt854@outlook.com>
2020-08-05 06:06:31 +0800

    btm of card

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBYM7k75o7lBvpqz9-FqpE7IzKpVAK4G_s',
+  authDomain: 'tiktok-clone-5ced4.firebaseapp.com',
+  databaseURL: 'https://tiktok-clone-5ced4.firebaseio.com',
+  projectId: 'tiktok-clone-5ced4',
+  storageBucket: 'tiktok-clone-5ced4.appspot.com',
+  messagingSenderId: '679045724679',
+  appId: '1:679045724679:web:29cbcb2d1941c2e74bc8a2',
+  measurementId: 'G-5XTRVD793H',
+};
